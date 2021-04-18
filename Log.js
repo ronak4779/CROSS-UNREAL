a101af292538cb803efc64589f1f4e3f48ce5c38
ronak4779 <81635920+ronak4779@users.noreply.github.com>
2021-04-18 14:03:40 +0530

    Add files via upload
    UNREAL

---
+++ Log.js
@@ -0,0 +1,26 @@
+class Log{
+    constructor(speed){
+        this.x = random([100,300,500,700,900]);
+        this.y = height-random([550,650,750,1350,1450,1550]);
+        this.width = 1500;
+        this.height = 800;
+        this.speed = speed;
+        this.spt = createSprite(this.x,this.y,this.width,this.height);
+        this.spt.velocityX = this.speed;
+        this.spt.shapeColor = "brown";
+        this.spt.scale=0.1;
+
+        for(i=1;i<logGroup1.length;i++){
+            if(logGroup1[i].x<0)
+            {
+              logGroup1[i].x=width;
+            }
+          }
+        
+
+    }
+
+    
+   
+}
+
